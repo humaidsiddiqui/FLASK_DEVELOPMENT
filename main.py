b25d20c8bd841461755ca0cf0c819b17ac242abc
humaidsiddiqui <humaidsiddiqui@gmail.com>
2025-01-09 04:40:41 -0800

    new changes

--- main.py
+++ main.py
@@ -3,6 +3,4 @@ app=Flask(__name__)
 @app.route("/home")
 def hello():
         return "Hi FROM AWS SERVER RUNNING BEHIND NGINX PROXY SERVER"
-if(__name__=="__main__"):
-        app.run(host="0.0.0.0",port=8080)
-        
+#app.run(host="0.0.0.0",port=8080)
