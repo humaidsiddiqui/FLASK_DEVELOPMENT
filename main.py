23968e79255f6e6b491b3ee10b28c06eeb414e85
humaidsiddiqui <humaidsiddiqui@gmail.com>
2025-01-09 04:47:56 -0800

    added new changes

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from flask import Flask
 app=Flask(__name__)
 @app.route("/home")
 def hello():
+        print("welcome to the world of python")
         return "Hi FROM AWS SERVER RUNNING BEHIND NGINX PROXY SERVER"
 #app.run(host="0.0.0.0",port=8080)
